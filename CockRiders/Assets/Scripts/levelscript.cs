bc575bc3cf66d8d3c2cee532b2f20e7326361ca1
Wowo10 <wojtekwojtek529@gmail.com>
2017-07-01 17:24:05 +0200

    cock axes fix

---
+++ CockRiders/Assets/Scripts/levelscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelscript : MonoBehaviour {
+
+    public PlayerBehaviour[] players;
+    public Quiz quiz;
+        
+	void Start () {
+        quiz.Show();
+	}
+	
+	void Update () {
+		
+	}
+}
