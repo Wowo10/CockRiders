f310169bbd8131504e0811950e3668e4b5c0ba09
Liffes <woj.g95@gmail.com>
2017-07-01 20:10:31 +0200

    Cock shaking on collision

--- CockRiders/Assets/Scripts/PlayerBehaviour.cs
+++ CockRiders/Assets/Scripts/PlayerBehaviour.cs
@@ -10,8 +10,13 @@ public class PlayerBehaviour : MonoBehaviour
     public char currentanswer = '0';
     public float jumpHeight = 20.0f;
     public float jumpForce = 10.0f;
-    
-    public float groundLevelY;
+    private float groundLevelY;
+    public float knockedRotation = 30.0f;
+
+    private bool isKnocked = false;
+    private float maxVelocityTemp;
+    public float knockedTimer = 1.0f;
+    public bool isResetingLerp;
 
     string fire, jump, aans, bans, cans;
     Rigidbody2D rb;
@@ -28,7 +33,7 @@ public class PlayerBehaviour : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
 
         maxvelocity = speed * maxvelocity;
-
+        maxVelocityTemp = maxvelocity;
         rb.velocity = gameObject.transform.right * 0 * speed;
     }
 
@@ -54,14 +59,23 @@ public class PlayerBehaviour : MonoBehaviour
     private void OnObstacleHit()
     {
         Debug.Log(name + " is retarded");
-        //TODO
+        isKnocked = true;
+        Invoke("ResetRotation", 1.0f);
+        maxVelocityTemp = 0.5f * maxvelocity;
+    }
+
+    private void ResetRotation()
+    {
+        isKnocked = false;
+        isResetingLerp = true;
+        maxVelocityTemp = maxvelocity;
     }
 
     void FixedUpdate()
     {
         if (Input.GetButtonDown(fire))
         {
-            float brake = 1 - (rb.velocity.x / maxvelocity);
+            float brake = 1 - (rb.velocity.x / maxVelocityTemp);
             rb.velocity += Vector2.right * speed * brake;
         }
         else
@@ -88,8 +102,28 @@ public class PlayerBehaviour : MonoBehaviour
             transform.SetPositionAndRotation(new Vector3(transform.position.x, groundLevelY, transform.position.z), Quaternion.identity);
             rb.velocity = new Vector3(rb.velocity.x, 0, 0); ;
         }
+        transform.rotation = Quaternion.Slerp(Quaternion.Euler(new Vector3(0, 0, knockedRotation)), Quaternion.identity, knockedTimer);
 
-
+        if (isKnocked)
+        {
+            knockedTimer -= Time.deltaTime;
+            if (knockedTimer < 0.0f)
+            {
+                isKnocked = false;
+                isResetingLerp = true;
+            }
+        }
+        if (isResetingLerp)
+        {
+            if (knockedTimer < 1.0f)
+                knockedTimer += Time.deltaTime;
+            else
+            {
+                knockedTimer = 1.0f;
+                isResetingLerp = false;
+            }
+        }
+        
         if (Input.GetButtonDown(aans))
         {
             currentanswer = 'a';
